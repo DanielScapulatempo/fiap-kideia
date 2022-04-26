1274471a92e84ea527cfe525a8ffc33627fc9585
DanielTS85 <danielscapulatempo@gmail.com>
2022-04-26 18:35:38 -0300

    get /ProcessStatus

---
+++ KideiaWebAPI/KideiaWebAPI/Controllers/ProcessStatusController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KideiaWebAPI.Models;
+
+namespace KideiaWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProcessStatusController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Confirmando e-mail", "Pendente de assinatura", "Contrato Assinado", "Verificando Documentos", "Plano Selecionado"
+        };
+
+        [HttpGet]
+        public IEnumerable<ProcessStatus> Get()
+        {
+            var rng = new Random();
+            int count = 0;
+            return Enumerable.Range(0, 4).Select(index => new ProcessStatus
+            {
+                Date = DateTime.Now.AddDays(index),
+                Summary = Summaries[count++]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ KideiaWebAPI/KideiaWebAPI/Models/ProcessStatus.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KideiaWebAPI.Models
+{
+    public class ProcessStatus
+    {
+        public DateTime Date { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
